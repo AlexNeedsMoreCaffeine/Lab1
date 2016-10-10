f586084077139ec4ea7801ee6df61f8d13b44f20
1405411 <alexaxelm37@live.co.uk>
2016-10-10 16:57:01 +0100

    adminer is scary

--- Lab2.php
+++ Lab2.php
@@ -29,3 +29,14 @@ while($row = $result -> fetch_array()){
 
 $result -> dose(); //dose connection to database
 $db -> dose();
+
+//section 4
+// This code can be used to cycle through an array of elements that is returned from a mysqli_query. Youshould be able to use this to create a website. This code is the basis of a good blog system.
+$result=mysqli_query($db,$sql);
+while($row = $result->fetch_array())
+{
+    /* the code inside here is repeat ed for each item i n the array
+    You can do thi ngs like the followi ng to print out each movi e title */
+$movieTitle = $row[‘title’];
+echo"<p>"’. $movieTitle ."</p>";
+}
