e60c32d79b82da0dbc5e26ebf622e978b70ced4c
1405411 <alexaxelm37@live.co.uk>
2016-10-17 14:45:31 +0100

    adding ?username=derp to the url

--- helloPrinter.php
+++ helloPrinter.php
@@ -1,10 +1,14 @@
 /**
 Week4 Lab lecture
  */
+<!DOCTYPE html>
 <?php
 
 $username = $_GET["username"];
+echo $username;
 
-echo $username
+echo"<p>Hello " . $username . " </p>";
+
+echo"<p>Hello {$username} </p>";
 
 ?>
