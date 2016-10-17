b60eceeaafc2f0929ee99075871acb5d7dba6e97
1405411 <alexaxelm37@live.co.uk>
2016-10-17 14:54:28 +0100

    adding ?username=derp to the url

--- helloSelector.php
+++ helloSelector.php
@@ -15,6 +15,6 @@ echo"<p>Hello {$planet} </p>";
 
 
 
-//echo "   <a href=\\"http://alexcm3028.azurewebsites.net/helloPrinter.php?planet=earth\\">Earth</a>";
+echo "   <a href=\\"http://alexcm3028.azurewebsites.net/helloPrinter.php?planet=earth\\">Earth</a>";
 
 ?>
